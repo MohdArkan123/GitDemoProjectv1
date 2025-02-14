e74b4de92fc8352954a9f207d50b76f23183c0f1
Mohd Arkan <arkansaifi5436@gmail.com>
2025-02-14 10:59:14 +0530

    first commit....

---
+++ src/pack1/Demo1.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int n=5;
+		if(n%2==0) {
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}

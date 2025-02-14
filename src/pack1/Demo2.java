8a1c7130fffbe329487e9c8fcaef2065e67b9486
Mohd Arkan <arkansaifi5436@gmail.com>
2025-02-14 12:14:02 +0530

    commiting Demo2 to BranchX

---
+++ src/pack1/Demo2.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int n=6;
+		if(n%3==0) {
+			System.out.println("n is divided by 3");
+		}
+		else {
+			System.out.println("n is not divided by 3");
+		}
+
+	}
+
+}
